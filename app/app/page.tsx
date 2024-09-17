45bb38abfdbae119fc861c8a9fc7866d04b459c0
Theo Furtado Mauricio <58632127+theofurtado05@users.noreply.github.com>
2024-09-17 18:58:36 -0300

    .

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,9 +1,11 @@
 import { ChartStreamingCompanies } from "./_components/chart-streaming-companies";
+import { ChartTicketMedio } from "./_components/chart-ticket-medio";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-2 gap-2">
+    <div className="grid grid-cols-2 gap-2 p-4">
       <ChartStreamingCompanies/>
+      <ChartTicketMedio/>
     </div>
   );
 }
