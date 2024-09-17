069e6672984298359d472781b45dea7dd2313933
Theo Furtado Mauricio <58632127+theofurtado05@users.noreply.github.com>
2024-09-17 18:16:07 -0300

    dashboard

---
+++ app/app/_components/chart-streaming-companies.tsx
@@ -0,0 +1,141 @@
+"use client"
+
+import * as React from "react"
+import { TrendingUp } from "lucide-react"
+import { Label, Pie, PieChart } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+
+export const description = "A donut chart with text"
+
+const chartData = [
+  { browser: "Netflix", visitors: 27, fill: "var(--color-chrome)" },
+  { browser: "Prime Video", visitors: 18, fill: "var(--color-safari)" },
+  { browser: "HBO Max", visitors: 14, fill: "var(--color-firefox)" },
+  { browser: "Disney", visitors: 12, fill: "var(--color-edge)" },
+  { browser: "Globoplay", visitors: 10, fill: "var(--color-other)" },
+  { browser: "Start+", visitors: 8, fill: "var(--color-other2)" },
+  { browser: "Apple Tv+", visitors: 6, fill: "var(--color-other3)" },
+  { browser: "Others", visitors: 5, fill: "var(--color-other4)" },
+]
+
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  chrome: {
+    label: "Chrome",
+    color: "hsl(var(--chart-1))",
+  },
+  safari: {
+    label: "Safari",
+    color: "hsl(var(--chart-2))",
+  },
+  firefox: {
+    label: "Firefox",
+    color: "hsl(var(--chart-3))",
+  },
+  edge: {
+    label: "Edge",
+    color: "hsl(var(--chart-4))",
+  },
+  other: {
+    label: "Other",
+    color: "hsl(var(--chart-5))",
+  },
+  other2: {
+    label: "Other",
+    color: "hsl(var(--chart-6))",
+  },
+  other3: {
+    label: "Other",
+    color: "hsl(var(--chart-7))",
+  },
+  other4: {
+    label: "Other",
+    color: "hsl(var(--chart-8))",
+  },
+} satisfies ChartConfig
+
+export function ChartStreamingCompanies() {
+  const totalVisitors = React.useMemo(() => {
+    return chartData.reduce((acc, curr) => acc + curr.visitors, 0)
+  }, [])
+
+  return (
+    <Card className="flex flex-col">
+      <CardHeader className="items-center pb-0">
+        <CardTitle>Total de assinantes</CardTitle>
+        <CardDescription>Brasil</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[250px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData}
+              dataKey="visitors"
+              nameKey="browser"
+              innerRadius={60}
+              strokeWidth={5}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-foreground text-3xl font-bold"
+                        >
+                          {totalVisitors.toLocaleString()}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="fill-muted-foreground"
+                        >
+                          %
+                        </tspan>
+                      </text>
+                    )
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col gap-2 text-sm">
+        <div className="leading-none text-muted-foreground">
+          Mostrando o total de assinantes de streaming no Brasil
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
---
+++ app/app/page.tsx
@@ -0,0 +1,9 @@
+import { ChartStreamingCompanies } from "./_components/chart-streaming-companies";
+
+export default function Home() {
+  return (
+    <div className="grid grid-cols-2 gap-2">
+      <ChartStreamingCompanies/>
+    </div>
+  );
+}
